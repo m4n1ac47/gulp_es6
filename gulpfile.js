d5d3f7508ba3bfa156b14dd856de4b5b691f415c
m4n1ac47 <m4n1ac47@gmail.com>
2018-11-20 05:06:06 +0300

    update autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -106,7 +106,7 @@ gulp.task('styles', function() {
 		.pipe(precss.val(precss.option).on("error", notify.onError()))
 		.pipe(rename({ suffix: '.min', prefix : '' }))
 		.pipe(gcmq())
-		.pipe(autoprefixer(['last 2 versions']))
+		.pipe(autoprefixer(['> 1%', 'last 4 versions']))
 		.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 		.pipe(gutil.env.type === 'production' ? gutil.noop() : sourcemaps.write('.'))
 		.pipe(gulp.dest('app/assets/css'))	
